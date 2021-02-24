8f027d845fbbf715e420520243d6bae4bb978578
敦史 <midori06180410@gmil.com>
2021-02-24 21:37:44 +0900

    非同期実装

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel_#{params['room']}"
+    stream_for current_user.id
   end
 
   def unsubscribed
@@ -8,6 +9,20 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    Message.create! content: data['message'], user_id: current_user.id, room_id: params['room']
-  end
+    if data["content"]
+    Message.create! (
+      content: data['message'], 
+      user_id: current_user.id, 
+      room_id: params['room']
+    )
+    
+    RoomChannel.broadcast_to data["current_user_id"].to_i,
+    content: data["message"],
+    isCurrent_user: true
+
+    RoomChannel.broadcast_to data["!current_user_id"].to_i,
+        content: data["sentence"],
+        isCurrent_user: false
+   end
+ end
 end
